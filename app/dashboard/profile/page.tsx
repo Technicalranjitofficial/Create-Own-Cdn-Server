6ba138bc44bc774e19e553ed2155fff9b8b4535e
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-10-06 22:41:51 +0530

    cdn server

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,54 +1,67 @@
-"use client"
-import UploadToGitHubs from '@/components/UploadToGitHub';
-import { CustomSession, authOption } from '@/utils/authOptions';
-import { UploadFilesToGithub,createDirectory, getAllFilesFromGithub, getAllGithubRepos, initializeBaseRepos } from '@/serverActions/files';
-import { getServerSession } from 'next-auth'
-import { useSession } from 'next-auth/react';
+// "use client"
+// import UploadToGitHubs from '@/components/UploadToGitHub';
+// import { CustomSession, authOption } from '@/utils/authOptions';
+// import { UploadFilesToGithub,createDirectory, getAllFilesFromGithub, getAllGithubRepos, initializeBaseRepos } from '@/serverActions/files';
+// import { getServerSession } from 'next-auth'
+// import { useSession } from 'next-auth/react';
+// import React from 'react'
+
+// const page = () => {
+
+//     const {data:session}= useSession();
+//     // const auth =await getServerSession(authOption);
+//     // console.log(auth);
+//     console.log(session);
+//     const accessToken = session && (session as unknown as CustomSession);
+//     // console.log(session && (session as unknown as CustomSession).accessToken);
+//   return (
+//     <div>
+//       <button onClick={async()=>{
+//        const data = await getAllGithubRepos(`${accessToken?.accessToken}`);
+//        const decode = JSON.parse(data);
+//        console.log(decode);
+
+
+//       }}>Get User Profile</button>
+// <br />
+// <br />
+// {accessToken && <button onClick={async()=>{
+//     const data = await initializeBaseRepos(`${accessToken?.accessToken}`,"rdserver",session.user?.name as string);
+//     console.log(data);
+// }}>Initialize Your Repos </button>}
+
+// <br />
+
+// {accessToken && <UploadToGitHubs AccessToken={accessToken?.accessToken as string} repoName='rdserver' username={session?.user?.name as string}  />}
+
+// <br />
+// <br />
+// <button onClick={()=>{
+//     const data = createDirectory(accessToken?.accessToken as string,"rdserver",session?.user?.name as string,"surya");
+// }}>Create New Folder</button>
+
+// <br />
+// <br />
+// <button onClick={async()=>{
+//     const data = await getAllFilesFromGithub(accessToken?.accessToken as string,"rdserver",session?.user?.name as string);
+//     console.log(data);
+// }}>Get All Files</button>
+//     </div>
+
+
+//   )
+// }
+
+// export default page
+
+
 import React from 'react'
 
 const page = () => {
-
-    const {data:session}= useSession();
-    // const auth =await getServerSession(authOption);
-    // console.log(auth);
-    console.log(session);
-    const accessToken = session && (session as unknown as CustomSession);
-    // console.log(session && (session as unknown as CustomSession).accessToken);
   return (
     <div>
-      <button onClick={async()=>{
-       const data = await getAllGithubRepos(`${accessToken?.accessToken}`);
-       const decode = JSON.parse(data);
-       console.log(decode);
-
-
-      }}>Get User Profile</button>
-<br />
-<br />
-{accessToken && <button onClick={async()=>{
-    const data = await initializeBaseRepos(`${accessToken?.accessToken}`,"rdserver",session.user?.name as string);
-    console.log(data);
-}}>Initialize Your Repos </button>}
-
-<br />
-
-{accessToken && <UploadToGitHubs AccessToken={accessToken?.accessToken as string} repoName='rdserver' username={session?.user?.name as string}  />}
-
-<br />
-<br />
-<button onClick={()=>{
-    const data = createDirectory(accessToken?.accessToken as string,"rdserver",session?.user?.name as string,"surya");
-}}>Create New Folder</button>
-
-<br />
-<br />
-<button onClick={async()=>{
-    const data = await getAllFilesFromGithub(accessToken?.accessToken as string,"rdserver",session?.user?.name as string);
-    console.log(data);
-}}>Get All Files</button>
+      
     </div>
-
-
   )
 }
 
--- components/ProjectsSection/FolderCards.tsx
+++ components/ProjectsSection/FolderCards.tsx
@@ -13,7 +13,7 @@ const FolderCards = ({data}:{data:IRequestFiles[]}) => {
   return (
     <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  mb-20 gap-5 p-3'>
       {data.map((file,index)=>{
-        return <Link href={`/projects/${file.name}`}> <div className='h-auto rounded-md text-slate-200 flex justify-between items-center w-full bg-transparent border border-gray-700 p-2 mt-5'>
+        return <Link key={index} href={`/projects/${file.name}`}> <div className='h-auto rounded-md text-slate-200 flex justify-between items-center w-full bg-transparent border border-gray-700 p-2 mt-5'>
            <div className='flex flex-row justify-center items-center gap-2'>
             <AiFillFolder  className="w-10 h-10 text-slate-300"/>
             <span >{file.name}</span>
